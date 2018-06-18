095fabdc367c4fa3c0e41d0aa93573eb98fdf8a2
Robert Alexandersson <robert.alexandersson@gmail.com>
2018-06-18 16:14:22 +0200

    RC2 minor changes to solr assertion tool

--- plugin/inject-solr-plugin/src/main/java/org/hrodberaht/injection/plugin/junit/solr/SolrAssertions.java
+++ plugin/inject-solr-plugin/src/main/java/org/hrodberaht/injection/plugin/junit/solr/SolrAssertions.java
@@ -17,7 +17,10 @@
 package org.hrodberaht.injection.plugin.junit.solr;
 
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.request.QueryRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.client.solrj.response.SolrResponseBase;
 import org.apache.solr.client.solrj.response.UpdateResponse;
@@ -35,14 +38,38 @@ import static org.junit.Assert.assertTrue;
 public class SolrAssertions {
 
     private final SolrClient solr;
-
-    public SolrAssertions(SolrClient solr) {
-        this.solr = solr;
-    }
+    private final String username;
+    private final String password;
+    private final String collection;
 
     public void cleanDataFromCollection() throws IOException, SolrServerException {
         solr.deleteByQuery("*:*");
         solr.commit(true, true, false);
+    }
+
+    public void waitForAsyncCommit() {
+        // TODO: Async needs a callback solution
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public enum Status {OK, FAIL}
+
+    public SolrAssertions(SolrClient solr) {
+        this.solr = solr;
+        this.collection = null;
+        this.username = null;
+        this.password = null;
+    }
+
+    public SolrAssertions(SolrClient solr, String collection, String username, String password) {
+        this.solr = solr;
+        this.collection = collection;
+        this.username = username;
+        this.password = password;
     }
 
     public void assertAddDocument(Status status, SolrInputDocument documents) throws IOException, SolrServerException {
@@ -70,22 +97,43 @@ public class SolrAssertions {
         }
     }
 
+
     public QueryResponse find(String query, String filterQuery) throws IOException, SolrServerException {
         org.apache.solr.client.solrj.SolrQuery solrQuery = new org.apache.solr.client.solrj.SolrQuery()
                 .setQuery(query)
                 .addFilterQuery(filterQuery)
                 .setStart(0)
                 .setRows(10);
+        return processQuery(solrQuery);
+    }
+
+    private QueryResponse processQuery(SolrQuery solrQuery) throws SolrServerException, IOException {
+        if (username != null) {
+            QueryRequest request = new QueryRequest(solrQuery);
+            appendBasicAuth(request);
+            return request.process(solr, collection);
+        }
         return solr.query(solrQuery);
     }
 
+    private void appendBasicAuth(SolrRequest request) {
+        if (username != null) {
+            request.setBasicAuthCredentials(username, password);
+        }
+    }
+
     public QueryResponse find(String query) throws IOException, SolrServerException {
+        return find(query, 10);
+    }
+
+    public QueryResponse find(String query, int rows) throws IOException, SolrServerException {
         org.apache.solr.client.solrj.SolrQuery solrQuery = new org.apache.solr.client.solrj.SolrQuery()
                 .setQuery(query)
                 .setStart(0)
-                .setRows(10);
-        return solr.query(solrQuery);
+                .setRows(rows);
+        return processQuery(solrQuery);
     }
+
 
     public QueryResponse findFields(String query, String... fields) throws IOException, SolrServerException {
         org.apache.solr.client.solrj.SolrQuery solrQuery = new org.apache.solr.client.solrj.SolrQuery()
@@ -93,7 +141,16 @@ public class SolrAssertions {
                 .setQuery(query)
                 .setStart(0)
                 .setRows(10);
-        return solr.query(solrQuery);
+        return processQuery(solrQuery);
+    }
+
+    public QueryResponse findFields(String query, int rows, String... fields) throws IOException, SolrServerException {
+        org.apache.solr.client.solrj.SolrQuery solrQuery = new org.apache.solr.client.solrj.SolrQuery()
+                .setFields(fields)
+                .setQuery(query)
+                .setStart(0)
+                .setRows(rows);
+        return processQuery(solrQuery);
     }
 
     public void assertResponse(SolrResponseBase response) {
@@ -124,7 +181,8 @@ public class SolrAssertions {
                 .setQuery(query)
                 .setStart(0)
                 .setRows(0);
-        QueryResponse queryResponse = solr.query(solrQuery);
+
+        QueryResponse queryResponse = processQuery(solrQuery);
         if (status == OK) {
             assertTrue("Status not OK status " + queryResponse.getStatus() + " results: " +
                             count + " actual " + queryResponse.getResults().getNumFound(),
@@ -134,8 +192,6 @@ public class SolrAssertions {
             assertTrue("Status not FAILED", queryResponse.getStatus() > 0);
         }
     }
-
-    public enum Status {OK, FAIL}
 
 
 }
